b41b7a2ba40c7bac1e859ee2142be93935515be8
Manakal <manakal@randomsoftware.net>
2022-01-31 19:24:46 +0530

    Added Tables and data loading from the database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 
@@ -8,9 +8,36 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-angularbot';
-  items: Observable<any[]>;
-  constructor(firestore: AngularFirestore) {
-    this.items = firestore.collection('superheros').valueChanges();
+  dataSource : any;
+  private name: any;
+  private Inteligencename: any;
+  private Power: any;
+  constructor(private  store: AngularFirestore){}
+  ngOnInit(){
+    this.getAll();
   }
+  @ViewChild('btnShow')
+  btnShow!: ElementRef;
+
+  @ViewChild('btnClose')
+  btnClose!: ElementRef;
+
+  openDialog(){
+    this.btnShow.nativeElement.click();
+  }
+
+  closeDialog(){
+    this.btnClose.nativeElement.click();
+  }
+  getAll() {
+    this.store.collection('superheros').snapshotChanges().subscribe((response) => {
+      this.dataSource = response.map(item =>
+        Object.assign({id: item.payload.doc.id}, item.payload.doc.data())
+      );
+    });
+  }
+   add(){
+     this.store.collection('superheros').add({Inteligence:this.Inteligencename, name: this.name, Power : this.Power});
+     this.closeDialog();
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(environment.firebase,'fire-base-learn'),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+}
